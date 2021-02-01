c548580c541a4407a99ca7d2106896db0ced7301
Jake Goodwin <jakegoodwin@tutanota.com>
2021-02-01 02:28:53 -0800

    Started to work on functions

--- web_interface/index.php
+++ web_interface/index.php
@@ -17,111 +17,7 @@
 <!-- Latest compiled JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     
-	<style type="text/css">
-
-    </style>
-    
-    
-<style>
-.wrapper{
-    width: 70%;
-    margin:0 auto;
-}
-.page-header h2{
-    margin-top: 0;
-}
-
-#Employees {
-  font-family: Arial, Helvetica, sans-serif;
-  border-collapse: collapse;
-  box-shadow: 5px 5px 10px;
-  width: 100%;
-}
-
-#Employees td, #Employees th {
-  border: 1px solid #ddd;
-  padding: 8px;
-}
-
-#Employees tr:nth-child(even){background-color: #f2f2f2;}
-
-#Employees tr:hover {background-color: #ddd;}
-
-#Employees th {
-  padding-top: 12px;
-  padding-bottom: 12px;
-  text-align: left;
-  background-color: #2196f3;
-  color: white;
-  }
-  
-  .navMenu{
-  overflow:hidden;background-color:#2196f3
-  }
-  .navMenu a{
-  float:left;display:block;color:#f2f2f2;text-align:center;padding:14px 16px;text-decoration:none;font-size:17px
-  }
-  .navMenu a.active{
-  background-color:#25a186;color:#FFF
-  }
-  .navMenu .icon{display:none}.fa-bars:before{content:"\f0c9"}.navMenu .dropdown{float:left;overflow:hidden}
-  .navMenu .dropdown .dropbtn{
-    font-size:16px;
-    border:none;
-    outline:none;
-    color:#FFFFFF;
-    padding:14px 16px;
-    background-color:#2196f3;
-    font-family:inherit;
-    margin:0;
-    width:100%;
-    text-align:left
-    }
-    
-    .main_links{
-    background-color:#2196f3;
-    color:#FFFFFF;
-    line-height:1
-    }
-    
-    .main_links:hover{
-    background-color:#ffa726;
-    color:#FFFFFF
-    }
-    
-    .navMenu .navbar a:hover,.navMenu .dropdown:hover .dropbtn{
-    background-color:#ffa726;
-    color:#FFFFFF
-    }
-    
-    .navMenu .dropdown-content{
-    display:none;
-    position:absolute;
-    background-color:#000;
-    min-width:160px;
-    box-shadow:0 8px 16px 0 rgba(0,0,0,0.2);
-    z-index:1
-    }
-    
-    .navMenu .dropdown-content a{
-    float:none;
-    color:#FFF;
-    padding:12px 16px;
-    text-decoration:none;
-    display:block;
-    text-align:left;
-    background-color:#808040;
-    color:#000000
-    }
-    
-    .navMenu .dropdown-content a:hover{
-    background-color:#333;
-    color:#FFFFFF
-    }
-    
-    .navMenu .dropdown:hover .dropdown-content{display:block}@media screen and (max-width: 768px){.navMenu a:not(:first-child){display:none}.navMenu .dropdown{display:none}.navMenu a.icon{float:right;display:block}.navMenu.mobileView{position:relative}.navMenu.mobileView .icon{position:absolute;right:0;top:0}.navMenu.mobileView a{float:none;display:block;text-align:left}.navMenu.mobileView .dropdown{float:none;display:block;text-align:left}.navMenu .dropdown-content{position:relative}}
-  
-</style>
+<link rel="stylesheet" href="index.css">
 <!--
     <script type="text/javascript">
         $(document).ready(function(){ $('[data-toggle="tooltip"]').tooltip();});
@@ -136,6 +32,29 @@
         require_once "config.php";
 //		include "header.php";
 	?>
+
+
+<?php 
+/*
+
+*/
+function createEmployee() {
+  echo "Hello world!";
+}
+
+
+function deleteEmployee() {
+  echo "Hello world!";
+}
+
+
+function updateEmployee() {
+  echo "Hello world!";
+}
+
+
+?>
+
 
 	
 <div class='navMenu' id='navMenuId'>
@@ -180,30 +99,32 @@
                                     //Form section to toss data into creating a new record.
                                     echo "<tr>";
                                     echo '<form action="welcome.php" method="post">';
-                                        echo "<td> <input type='text' Ssn=ssn </td>";
-                                        echo "<td> <input type='text' firstName=firstName </td>";
-                                        echo "<td> <input type='text' lastName=lastName </td>";
-                                        echo "<td> <input type='text' Street=Street </td>";
-                                        echo "<td> <input type='text' State=State </td>";
-                                        echo "<td> <input type='text' Birthday=Birthday </td>";
-                                        echo "<td> <input type='text' Salary=Salary </td>";
+                                        echo "<td> <input type='text' Ssn=ssn > </input> </td>";
+                                        echo "<td> <input type='text' firstName=firstName  </input> </td>";
+                                        echo "<td> <input type='text' lastName=lastName  </input> </td>";
+                                        echo "<td> <input type='text' Street=Street  </input> </td>";
+                                        echo "<td> <input type='text' State=State  </input> </td>";
+                                        echo "<td> <input type='text' Birthday=Birthday  </input> </td>";
+                                        echo "<td> <input type='text' Salary=Salary  </input> </td>";
                                         echo '<td> <a href="createEmployee.php" class="btn btn-success pull-right">Add</a> </td>';
                                     echo '</form>';
                                     echo "</tr>";
                                     //loop through all rows.
                                     while($row = mysqli_fetch_row($result))
                                     {
-                                    echo "<tr>";
-                                        echo "<td>" . $row[0] . "</td>";
-                                        echo "<td>" . $row[1] . "</td>";
-                                        echo "<td>" . $row[2] . "</td>";
-                                        echo "<td>" . $row[3] . "</td>";
-                                        echo "<td>" . $row[4] . "</td>";
-                                        echo "<td>" . $row[5] . "</td>";
-                                        echo "<td>" . $row[6] . "</td>";
-                                        echo '<td>  <a href="createEmployee.php" class="btn btn-success pull-right">DELETE</a> </td>'; 
-                                        
-                                    echo "</tr>";
+                                    echo '<tr> <form action="updateEmployee.php" method="post">';
+                                        echo "<td> <input type='text' value=" . $row[0] . "></input></td>";
+                                        echo "<td> <input type='text' value=" . $row[1] . "></input> </td>";
+                                        echo "<td> <input type='text' value=" . $row[2] . "></input> </td>";
+                                        echo "<td> <input type='text' value=" . $row[3] . "></input> </td>";
+                                        echo "<td> <input type='text' value=" . $row[4] . "></input> </td>";
+                                        echo "<td> <input type='text' value=" . $row[5] . "></input> </td>";
+                                        echo "<td> <input type='text' value=" . $row[6] . "></input> </td>";
+                                        echo '<td>
+                                        <a href="createEmployee.php" class="btn btn-success pull-right">DELETE</a>
+                                        <a href="createEmployee.php" class="btn btn-success pull-right">UPDATE</a> 
+                                        </td>'; 
+                                    echo "</tr> </form>";
                                     
                                     }
                                     
