396b44df1cce70c1c43d4c96d2d8df63c2576e96
Jake Goodwin <jakegoodwin@tutanota.com>
2021-02-01 01:16:15 -0800

    Added some cool styling to the page. Index is now set up with form to create new records on the page.

--- web_interface/departmentPage.php
+++ web_interface/departmentPage.php
@@ -29,33 +29,6 @@
             margin-right: 15px;
         }
     </style>
-    
-    
-<style>
-#Employees {
-  font-family: Arial, Helvetica, sans-serif;
-  border-collapse: collapse;
-  box-shadow: 5px 5px 10px;
-  width: 100%;
-}
-
-#Employees td, #Employees th {
-  border: 1px solid #ddd;
-  padding: 8px;
-}
-
-#Employees tr:nth-child(even){background-color: #f2f2f2;}
-
-#Employees tr:hover {background-color: #ddd;}
-
-#Employees th {
-  padding-top: 12px;
-  padding-bottom: 12px;
-  text-align: left;
-  background-color: #2196f3;
-  color: white;
-}
-</style>
 <!--
     <script type="text/javascript">
         $(document).ready(function(){ $('[data-toggle="tooltip"]').tooltip();});
@@ -76,9 +49,7 @@
                 <div class="col-md-12">
                     <div class="page-header clearfix">
                         <h2 class="pull-left">Employee Details</h2>
-                        <!--
                         <a href="createEmployee.php" class="btn btn-success pull-right">Add New Employee</a>
-                        -->
                     </div>
                     <?php
                     // Include config file
@@ -88,7 +59,7 @@
                     $sql = "SELECT Ssn, FirstName, LastName, Street, State, Birthday, Salary FROM EMPLOYEE;";
                     if($result = mysqli_query($link, $sql)){
                         if(mysqli_num_rows($result) > 0){
-                            echo "<table id='Employees' class='table table-bordered table-striped'>";
+                            echo "<table class='table table-bordered table-striped'>";
                                 echo "<thead>";
                                     echo "<tr>";
                                         echo "<th width=8%>Ssn</th>";
@@ -96,24 +67,11 @@
                                         echo "<th width=10%>Last Name</th>";
                                         echo "<th width=10%>Street </th>";
                                         echo "<th width=10%>State </th>";
-                                        echo "<th width=10%>Birthday </th>";
-                                        echo "<th width = 5%>Salary</th>";
+										echo "<th width=10%>Birthday </th>";
+										echo "<th width = 5%>Salary</th>";
                                         echo "<th width=10%>Action</th>";
                                     echo "</tr>";
                                     
-                                    //Form section to toss data into creating a new record.
-                                    echo "<tr>";
-                                    echo '<form action="welcome.php" method="post">';
-                                        echo "<td> <input type='text' Ssn=ssn </td>";
-                                        echo "<td> <input type='text' firstName=firstName </td>";
-                                        echo "<td> <input type='text' lastName=lastName </td>";
-                                        echo "<td> <input type='text' Street=Street </td>";
-                                        echo "<td> <input type='text' State=State </td>";
-                                        echo "<td> <input type='text' Birthday=Birthday </td>";
-                                        echo "<td> <input type='text' Salary=Salary </td>";
-                                        echo '<td> <input type="submit" value="Create"></td>';
-                                    echo '</form>';
-                                    echo "</tr>";
                                     //loop through all rows.
                                     while($row = mysqli_fetch_row($result))
                                     {
@@ -143,6 +101,44 @@
                     } else{
                         echo "ERROR: Could not able to execute $sql. <br>" . mysqli_error($link);
                     }
+					echo "<br> <h2> Department Stats </h2> <br>";
+					
+                    // Select Department Stats
+					/*
+                    $sql2 = "SELECT * FROM DeptSTATS";
+                    if($result2 = mysqli_query($link, $sql2)){
+                        if(mysqli_num_rows($result2) > 0){
+                            echo "<div class='col-md-4'>";
+							echo "<table width=30% class='table table-bordered table-striped'>";
+                                echo "<thead>";
+                                    echo "<tr>";
+                                        echo "<th width=20%>Dnum</th>";
+                                        echo "<th width = 20%>Number of Employees</th>";
+                                        echo "<th width = 40%>Average Salary Basepay</th>";
+	
+                                echo "</thead>";
+                                echo "<tbody>";
+                                while($row = mysqli_fetch_array($result2)){
+                                    echo "<tr>";
+                                        echo "<td>" . $row['Dnum'] . "</td>";
+                                        echo "<td>" . $row['EmpCount'] . "</td>";
+                                        //Going to be used for basepay
+                                        echo "<td>" . $row['AvgSalary'] . "</td>";
+               
+                                    echo "</tr>";
+                                }
+                                echo "</tbody>";                            
+                            echo "</table>";
+                            // Free result set
+                            mysqli_free_result($result2);
+                        } else{
+                            echo "<p class='lead'><em>No records were found for Dept Stats.</em></p>";
+                        }
+                    } else{
+                        echo "ERROR: Could not able to execute $sql2. <br>" . mysqli_error($link);
+                    }
+					*/
+                    // Close connection
                     mysqli_close($link);
                     ?>
                 </div>
