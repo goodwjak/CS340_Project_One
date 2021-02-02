eca274c1bfe784b8e4d35f053997fccbef7cb8af
jerred-shifflett <jerredshifflett@gmail.com>
2021-02-01 16:16:10 -0800

    much better tables

--- Jerreds_playground/Prototype1.0/company.sql
+++ Jerreds_playground/Prototype1.0/company.sql
@@ -53,11 +53,12 @@ create table Pay(
 --dFullname may solve this already
 create table Dependent (
   Essn char(9) not null,  
-  DependentName varchar(15) not null,
+  FirstName varchar(15) not null,
+  LastName varchar(15) not null,
   BirthDate date not null,
   Address varchar(30) not null,
   constraint DFullname UNIQUE(FirstName, LastName)
-  primary key (Essn, DependentName),
+  primary key (Essn, FirstName, LastName),
   foreign key (Essn) references Employee(Ssn)
 )ENGINE = INNODB; 
 
@@ -72,9 +73,7 @@ create table DeptLOCATIONS (
 
 
 
---filling the tables brah
-
--- we need nine entries for now
+-- name,num,manager
 insert into Department values 
  ('BioEngineering',1,111111111),
  ('Robotics',2,222222222),
@@ -88,24 +87,31 @@ insert into Department values
 
 
 
-  FirstName,
-  LastName,
-  Ssn,  
-  BirthDate ,
-  Address ,
-  Salary ,
-  SuperSsn ,
-  Dnum,
---we i init the tables
+ -- FirstName,LastName,Ssn,BirthDate,Address,Salary,SuperSsn,Dnum,
+--we  init the tables
 insert into Employee values 
- ('John','Smith',123456789,'1965-01-09','731 Fondren, Houston TX',70000,333445555,5),
- ('Franklin','Wong',333445555,'1965-12-08','638 Voss, Houston TX',70000,888665555,5),
- ('Alicia','Zelaya',999887777,'1968-01-19','3321 Castle, Spring TX',70000,987654321,4),
- ('Jennifer','Wallace',987654321,'1941-06-20','291 Berry, Bellaire TX',70000,888665555,4),
- ('Ramesh','Narayan',666884444,'1962-09-15','975 Fire Oak, Humble TX',70000,333445555,5),
- ('Joyce','English',453453453,'1972-07-31','5631 Rice, Houston TX',70000,333445555,5),
- ('Ahmad','Jabbar',987987987,'1969-03-29','980 Dallas, Houston TX',70000,987654321,4),
- ('James','Borg',888665555,'1937-11-10','450 Stone, Houston TX',70000,null,1);
+-- The Department Heads- They report to Admin
+ ('Jack','Low',111111111,'1989-07-09','420 Record Way, Portland OR',75000,987654321,1),
+ ('Ann','Red',222222222,'1978-09-11','89 This Street, Salem OR',75000,987654321,2),
+ ('Alex','Gaya',333333333,'1990-11-18','455 Castle BLVD., Corvallis OR',75000,987654321,3),
+ ('Charles','Neugen',444444444,'1989-10-23','178 Home Place, Eugene OR',75000,987654321,4),
+ ('Joy','Howl',555555555,'1976-02-11','765 Around BLVD, Bend OR',75000,987654321,5),
+ ('Bill','Crew',666666666,'1987-08-21','899 Younder Way, Newport OR',75000,987654321,6),
+ ('Jill','Jackson',777777777,'1998-06-30','342 Goose St., Albany OR',75000,987654321,7),
+ ('Zaya','Meta',987654321,'1998-06-30','980 UpHigh Road, Mt. Hood OR',75000,987654321,8),
+ --Top BOSS
+ ('Jesse','Jesse',123456789,'1999-10-18','908 Water Ave., SeaSide OR',75000,987654321,9),
+ --Regular Employees
+  ('Jane','Hill',123123123,'1978-04-30','888 Road Rd., Portland OR',60000,111111111,1),
+  ('Bob','Apples',321321321,'1908-01-01',' 444 Street St., Salem OR',60000,222222222,2),
+  ('Jackie','Wong',321123321,'1970-01-01','555 Parkway Prkwy., Corvallis OR',60000,333333333,3),
+  ('Harold','Joules',234234234,'2000-01-90','888 House Rd., Eugene OR',60000,444444444,4),
+  ('Sawn','Tells',345345345,'2003-04-19',' 999 Long St., Bend OR',60000,555555555,5),
+  ('Nick','Knack',456456456,'2001-12-31',' 103 Lane Ln., Newport OR',60000,666666666,6),
+  ('Patty','Wack',567567567,'2000-19-22','344 Left Rd., Albany OR',60000,777777777,7),
+  ('Eric','Cire',678678678,'1999-11-30',' 455 Right Rd., Portland OR',60000,111111111,1);
+
+
 --projectName, Pnum, Location, Department
 insert into Project values 
  ('RoboticSnake',1,'Portland',1),
@@ -115,34 +121,35 @@ insert into Project values
  ('Roover',5,'Bend',5),
  ('SamplingAutomation',6,'Bend',5),
  ('Lazer',7,'Corvallis', 3);
---add more than one project to a person
+
+
+
+-- Essn,Pnum,Hours, Projects,
+--add more than one project to a person via webinterface
 insert into WorksOn values 
- (123456789,1,32.5),
- (123456789,2,7.5),
- (666884444,3,40.0),
- (453453453,1,20.0),
- (453453453,2,20.0),
- (333445555,2,10.0),
- (333445555,3,10.0),
- (333445555,5,10.0),
- (333445555,6,10.0),
- (999887777,7,30.0),
- (999887777,3,10.0),
- (987987987,4,35.0),
- (987987987,3,5.0),
- (987654321,1,20.0),
- (987654321,2,15.0),
- (888665555,2,null);
+ (123123123,1,32.5),
+ (321321321,2,7.5),
+ (321123321,3,40.0),
+ (234234234,4,20.0),
+ (345345345,5,20.0),
+ (456456456,6,10.0),
+ (567567567,7,10.0),
+ (123123123,2,8.0),
+ (678678678,1,10.0);
 
+
+
+
+-- Essn, FirstName, LastName, BirthDate, Address
 insert into Dependent values 
- (333445555,'Alice','1986-04-04','Child'),
- (333445555,'Theodore','1983-10-25','Child'),
- (333445555,'Joy','1958-05-03','Spouse'),
- (987654321,'Abner','1942-02-28','Spouse'),
- (123456789,'Michael','1988-01-04','Child'),
- (123456789,'Alice','1988-12-30','Child'),
- (123456789,'Elizabeth','1967-05-05','Spouse');
-
+ (123123123,'Apple','Tree','2010-04-05','Child'),
+ (123123123,'Tom','Space', '2013-11-15','Child'),
+ (321321321,'Ben','Gown','1990-11-23','Spouse'),
+ (234234234,'Mike','Jackson','1978-08-18','Spouse'),
+ (345345345,'Michael','Fox','2004-06-05','Child'),
+ (456456456,'Alice','Wonderland','2018-11-21','Child'),
+ (567567567,'Elizabeth','Beth','2000-03-06','Spouse');
+-- Dep, LocationName
 insert into DeptLOCATIONS values
  (1,'Portland'),
  (2,'Salem'),
