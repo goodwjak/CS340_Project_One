56237419552b0ee546a1d3d79c16c23208c297e7
Jake Goodwin <jakegoodwin@tutanota.com>
2021-03-15 15:36:21 -0700

    Working on makeing a page back function.

--- web_interface/updateEmployee.php
+++ web_interface/updateEmployee.php
@@ -12,16 +12,34 @@ include_once 'config.php';
 include("loading_animation.html");
 
 echo "<p>Modifing the employee</p>";
-echo "<script>console.log('Adding EMPLOYEE');</script>";
+echo "<script>console.log('Updating employee');</script>";
 
 //echo "<script>console.log('" . $_POST['Ssn'] . "');</script>";
 
 //check to see if we are updating or deleteing a employee.
 if($_POST['action'] == "DELETE"){
     //call the delete function
+    delete_employee();
 } else if($_POST['action'] == "UPDATE"){
     //call the update function
+    update_employee();
 }
+
+//here I need to have it send the client back to the previous page and load it.
+echo "<script>console.log('Script Done');</script>";
+
+//pause for a couple seconds.
+sleep(5);
+
+//goback to the main page
+//header('Location: index.php');
+
+
+/*
+#######################################
+FUNCTIONS
+#######################################
+*/
 
 
 /*
@@ -31,7 +49,7 @@ Description: Deletes a employee from the database.
 */
 function delete_employee() {
     //first make sure the data isn't empty and that we have the key.
-    if(isset($_POST['Ssn'])) {
+    if(isset($_POST['Ssn']) and strlen($_POST['Ssn']) >= 8) {
         //get the key value.
         $Ssn = $_POST['Ssn'];
     
@@ -41,20 +59,21 @@ function delete_employee() {
         //send the sql.
         if (mysqli_query($link, $sql)) {
             //Tell the user that it war good.
-            echo "<script>console.log('Sucess adding employee.');</script>";
+            echo "<script>console.log('Sucess sending data.');</script>";
         } 
         else {
             //Tell the user there war a error.
             echo "<script>console.log('Error: " . $sql . ":-" . mysqli_error($link) . ");</script>";
             
             //snap back
+            echo '<script>alert("ERROR: Had trouble with the database!");</script>';
         }
     
     } 
     //otherwise we can just log the error to clonsole and alert the user.
     else {
         //give error
-        
+        echo '<script>alert("ERROR: Missing Data!");</script>';
         //snap back
     
     }
@@ -69,7 +88,7 @@ Description: Updates the values of an employee in the database
 */
 function update_employee() {
     //first make sure the data isn't empty and that we have the key.
-    if(isset($_POST['Ssn'])) {
+    if(isset($_POST['Ssn']) and strlen($_POST['Ssn']) >= 8) {
         //get the key value.
         $Ssn = $_POST['Ssn'];
     
@@ -79,7 +98,7 @@ function update_employee() {
         //send the sql.
         if (mysqli_query($link, $sql)) {
             //Tell the user that it war good.
-            echo "<script>console.log('Sucess adding employee.');</script>";
+            echo "<script>console.log('Sucess sending data');</script>";
         } 
         else {
             //Tell the user there war a error.
@@ -92,13 +111,11 @@ function update_employee() {
     //otherwise we can just log the error to clonsole and alert the user.
     else {
         //give error
-        
+        echo '<script>alert("ERROR: Missing Data!");</script>';
         //snap back
     
     }
 }
 
-//here I need to have it send the client back to the previous page and load it.
-echo "<script>console.log('Script Done');</script>";
-//header('Location: index.php');
+
 ?>
